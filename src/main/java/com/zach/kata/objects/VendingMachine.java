9ae45f0f1c452a83f6a39ba4ded8bd4a4be6702d
zstuder25 <zstuder25@gmail.com>
2017-12-03 21:34:17 -0500

    Insert a nickel and get display back

--- src/main/java/com/zach/kata/objects/VendingMachine.java
+++ src/main/java/com/zach/kata/objects/VendingMachine.java
@@ -1,5 +1,9 @@
 package com.zach.kata.objects;
 
+
+import static com.zach.kata.constants.Constants.Coin.NICKEL_D;
+import static com.zach.kata.constants.Constants.Coin.NICKEL_W;
+
 /**
  * Created by zmans on 12/3/2017.
  */
@@ -8,6 +12,9 @@ public class VendingMachine {
 
 
     public String insertCoin(Coin coin){
+        if(coin.getCoinWeight() == NICKEL_W && coin.getCoinDiameter() == NICKEL_D){
+            return("$0.05");
+        }
         return "";
     }
 }
--- src/test/java/com/zach/kata/objects/VendingMachineTest.java
+++ src/test/java/com/zach/kata/objects/VendingMachineTest.java
@@ -2,6 +2,8 @@ package com.zach.kata.objects;
 
 import org.junit.Test;
 
+import static com.zach.kata.constants.Constants.Coin.NICKEL_D;
+import static com.zach.kata.constants.Constants.Coin.NICKEL_W;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,5 +15,11 @@ public class VendingMachineTest {
     public void acceptCoinTest(){
         VendingMachine vendingMachine = new VendingMachine();
         assertEquals("", vendingMachine.insertCoin(new Coin(0, 0)));
+    }
+
+    @Test
+    public void insertValidCoinNickel(){
+        VendingMachine vendingMachine = new VendingMachine();
+        assertEquals("$0.05", vendingMachine.insertCoin(new Coin(NICKEL_W, NICKEL_D)));
     }
 }
