24a8ef74dcab2e2e5a4d512834d553ba7f9db4d6
zstuder25 <zstuder25@gmail.com>
2017-12-03 20:01:12 -0500

    Make a coin test passed

---
+++ src/test/java/com/zach/kata/objects/CoinTest.java
@@ -0,0 +1,17 @@
+package com.zach.kata.objects;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+
+/**
+ * Created by zmans on 12/3/2017.
+ */
+public class CoinTest {
+
+    @Test
+    public void makeACoinTest(){
+        Coin coin = new Coin();
+        assertNotNull(coin);
+    }
+}
